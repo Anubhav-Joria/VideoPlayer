621ab60ac069dfaacb428af57414e393c2956d34
Anubhav-Joria <91547784+Anubhav-Joria@users.noreply.github.com>
2023-04-07 18:55:08 +0530

    Navbar improved

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,30 +3,29 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
-import { Link } from "react-router-dom";
 export default function ButtonAppBar() {
+  const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, color: "white" }}
+            sx={{ color: "#ffffff", cursor: "pointer" }}
+            onClick={() => navigate("/")}
           >
-            <Link to="/" color="inherit">
-              Conwin
-            </Link>
+            Video Player
           </Typography>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, color: "white" }}
+            sx={{ color: "#ffffff", cursor: "pointer" }}
+            onClick={() => navigate("/add")}
           >
-            <Link to="/add" color="inherit">
-              Add Entry
-            </Link>
+            Add Entry
           </Typography>
         </Toolbar>
       </AppBar>
